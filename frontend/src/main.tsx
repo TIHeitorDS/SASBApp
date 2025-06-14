8d6f1736079b8a67134b723eb5ea2acee76bd8df
TIHeitorDS <heitorclaudinods@gmail.com>
2025-06-14 18:44:49 -0300

    feat: add routing for login and signup pages with react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Login from "./pages/login.tsx";
+import SignUp from "./pages/signup.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
