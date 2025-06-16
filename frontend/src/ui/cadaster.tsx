c2dcf230db6de102aa3e28adce7f9b9f254ae3f6
TIHeitorDS <heitorclaudinods@gmail.com>
2025-06-16 14:59:23 -0300

    feat: add logout link to Navbar and improve layout in Cadaster component

--- frontend/src/ui/cadaster.tsx
+++ frontend/src/ui/cadaster.tsx
@@ -9,13 +9,15 @@ export default function Layout({
 }) {
   return (
     <div className="flex flex-col justify-center items-center gap-[27px]">
-      <p className="font-syne font-semibold text-4xl">{title}</p>
+      <p className="font-syne font-semibold text-2xl text-center lg:text-4xl">
+        {title}
+      </p>
 
-      <form action="" className="grid grid-cols-2 w-full gap-[27px]">
+      <form action="" className="grid lg:grid-cols-2 w-full gap-[27px]">
         {children}
       </form>
-      
-      <div className="w-50 mr-auto">
+
+      <div className="w-full lg:w-50 mr-auto">
         <SubmitButton text="Cadastrar" theme="black" />
       </div>
     </div>
