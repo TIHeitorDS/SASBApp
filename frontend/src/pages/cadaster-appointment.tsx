fee4e844d9d50e30c395bf4674025da9153425a0
TIHeitorDS <heitorclaudinods@gmail.com>
2025-06-16 14:28:39 -0300

    feat: add SelectService and SelectWorker components to CadasterAppointment page

--- frontend/src/pages/cadaster-appointment.tsx
+++ frontend/src/pages/cadaster-appointment.tsx
@@ -1,4 +1,6 @@
 import Input from "../components/input";
+import SelectService from "../components/select-service";
+import SelectWorker from "../components/select-worker";
 import Layout from "../ui/cadaster";
 
 export default function CadasterAppointment() {
@@ -7,9 +9,9 @@ export default function CadasterAppointment() {
       <Input type="text" placeholder="Cliente" theme="black" />
       <Input type="email" placeholder="Email" theme="black" />
       <Input type="text" placeholder="Telefone" theme="black" />
-      <Input type="text" placeholder="Selecionar Serviço" theme="black" />
+      <SelectService />
       <Input type="text" placeholder="Selecionar Data" theme="black" />
-      <Input type="email" placeholder="Selecionar profissional" theme="black" />
+      <SelectWorker />
     </Layout>
   );
 }
