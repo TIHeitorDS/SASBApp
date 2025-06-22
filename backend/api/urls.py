2c8fd31e2c5cbbceebfdd947b113213f3e576adc
JhoanDev <jhoandev.log@gmail.com>
2025-06-21 22:41:34 -0300

    refact: ajustando urls para ao inves de ser colaborador ser funcionario.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,14 +1,12 @@
-# api/urls.py
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AdministratorViewSet, CollaboratorViewSet, ServiceViewSet, AppointmentViewSet, UserViewSet
+from .views import AdministratorViewSet, EmployeeViewSet, ServiceViewSet, AppointmentViewSet, UserViewSet
 
 router = DefaultRouter()
 
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'administrators', AdministratorViewSet, basename='administrator')
-router.register(r'collaborators', CollaboratorViewSet, basename='collaborator')
+router.register(r'employees', EmployeeViewSet, basename='employee')  
 router.register(r'services', ServiceViewSet, basename='service')
 router.register(r'appointments', AppointmentViewSet, basename='appointment')
 
