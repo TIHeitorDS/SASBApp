467409d2c5fb52dbbe2bae6ba3860037034d8a17
TIHeitorDS <heitorclaudinods@gmail.com>
2025-06-24 08:45:34 -0300

    refactor: altera quantidade de colunas no formulario de cadastrar servico

--- frontend/src/pages/cadaster-service.tsx
+++ frontend/src/pages/cadaster-service.tsx
@@ -3,7 +3,7 @@ import Layout from "../ui/cadaster";
 
 export default function CadasterService() {
   return (
-    <Layout title="Cadastrar Novo Serviço">
+    <Layout title="Cadastrar Novo Serviço" tableColumns="3">
       <Input type="text" placeholder="Nome do Serviço" theme="black" />
 
       <Input type="text" placeholder="Duração" theme="black" />
