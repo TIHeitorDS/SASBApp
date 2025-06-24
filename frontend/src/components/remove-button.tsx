9f96a5d2f0a1bc7502b0317a94a0f595a19d0da9
TIHeitorDS <heitorclaudinods@gmail.com>
2025-06-24 08:37:08 -0300

    feat: cria os botoes de editar e remover

---
+++ frontend/src/components/remove-button.tsx
@@ -0,0 +1,10 @@
+export default function RemoveButton() {
+  return (
+    <button
+      type="button"
+      className="bg-black text-white rounded-[2px] py-[10px] px-[32.5px] font-semibold"
+    >
+      Remover
+    </button>
+  );
+}
