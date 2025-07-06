f0e3036b4246ed4b66d5c5d747a2253c7a57cbb7
dayvisonmsilva <dayvisoneryc@gmail.com>
2025-07-06 01:26:45 -0300

    feat(auth): Implementação da integração da página de login com o back-end

--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -2,10 +2,14 @@ export default function Input({
   type,
   placeholder = "",
   theme = "white",
+  value,
+  onChange,
 }: {
   type: "email" | "password" | "text";
   placeholder: string;
   theme?: "white" | "black";
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) {
   const borderType = theme === "white" ? "border-white/25" : "border-[#c4c4c4]";
   const textColor = theme === "white" ? "text-white" : "text-[#767575]";
@@ -14,6 +18,8 @@ export default function Input({
     <input
       type={type}
       placeholder={placeholder}
+      value={value}
+      onChange={onChange}
       className={`border pt-[15px] pb-[17px] pl-[21px] ${borderType} w-full focus:outline-none focus:border-pink focus:ring-1 focus:ring-pink rounded bg-transparent ${textColor} placeholder:${textColor}/25 transition-all`}
     />
   );
